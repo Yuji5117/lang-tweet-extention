0a99f16f4da7d4e93756022cdad09442e13e82eb
Yuji Nakamura <ny5117@icloud.com>
2022-01-15 11:16:28 +0900

    ステータスバーの実装

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import "./App.css";
 
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Alert from "@mui/material/Alert";
 
 function App() {
   const [tweetKr, setTweetKr] = useState("");
@@ -11,12 +14,18 @@ function App() {
   const [textFormat, setTextFormat] = useState(
     `＜簡単表現＞\n\n【韓国語】\n・${tweetKr}\n\n【日本語】\n・${tweetJp}\n\n#韓国語 #ハングル #korean #勉強`
   );
+  const [tempNum, setTempNum] = useState(1);
+  const [message, setMessage] = useState("");
+
+  const templates = [
+    `＜簡単表現＞\n\n【韓国語】\n・${tweetKr}\n\n【日本語】\n・${tweetJp}\n\n#韓国語 #ハングル #korean #勉強`,
+    `＜一言フレーズ一覧＞\n\n【日本語】\n・${tweetKr}\n\n【韓国語】\n・${tweetJp}`,
+    `＜長文＞\n\n【日本語】\n・${tweetKr}\n\n【韓国語】\n・${tweetJp}`,
+  ];
 
   useEffect(() => {
-    setTextFormat(
-      `＜簡単表現＞\n\n【韓国語】\n・${tweetKr}\n\n【日本語】\n・${tweetJp}\n\n#韓国語 #ハングル #korean #勉強`
-    );
-  }, [tweetKr, tweetJp]);
+    setTextFormat(templates[tempNum]);
+  }, [tweetKr, tweetJp, tempNum]);
 
   const onHandleTweetValue = (e: any) => {
     e.preventDefault();
@@ -30,25 +39,27 @@ function App() {
     }
   };
 
+  const handleChangeTemplate = (e: any) => {
+    console.log(textFormat);
+    setTempNum(e.target.value);
+  };
+
   const onTweet = async () => {
     const endpoint = `${process.env.REACT_APP_API_ENDPOINT_URL}/twitter-manager`;
 
     try {
       const res = await axios.post(endpoint, textFormat);
-      return {
-        statusCode: 200,
-        body: JSON.stringify(res),
-      };
+      setMessage(res.data);
     } catch (e) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify(e),
-      };
+      setMessage("ツイートに失敗しました");
     }
   };
 
   return (
     <div className="App">
+      <div className="header_label">
+        <h1 className="header_title">Lang Tweet</h1>
+      </div>
       <form className="tweet_form" action="">
         <div className="tweet_input_box">
           <TextField
@@ -80,6 +91,28 @@ function App() {
           </Button>
         </div>
       </form>
+      <Select
+        labelId="demo-simple-select-autowidth-label"
+        id="demo-simple-select-autowidth"
+        value={tempNum}
+        onChange={handleChangeTemplate}
+        autoWidth
+        label="Format"
+      >
+        <MenuItem value={1}>簡単表現</MenuItem>
+        <MenuItem value={2}>一言フレーズ一覧</MenuItem>
+        <MenuItem value={3}>長文</MenuItem>
+      </Select>
+      {message === "ツイートされました！！" && (
+        <div className="status_bar">
+          <Alert severity="success">{message}</Alert>
+        </div>
+      )}
+      {message === "ツイートに失敗しました" && (
+        <div className="status_bar">
+          <Alert severity="error">{message}</Alert>
+        </div>
+      )}
     </div>
   );
 }
