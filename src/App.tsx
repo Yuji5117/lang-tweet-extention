223b97e95927b08bcc5239bb375d9435eb156901
Yuji Nakamura <ny5117@icloud.com>
2022-01-30 19:43:41 +0900

    ライク機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import TweetForm from "./components/TweetForm";
 import Header from "./components/Header";
 import Alert from "@mui/material/Alert";
+import axios from "axios";
 
 interface DefaultValues {
   tweetKr: string;
@@ -18,16 +19,38 @@ const App: React.FC = () => {
     tweetJp: "",
   };
 
+  const onLikeTweet = async () => {
+    const endpoint = `http://localhost:3000/likes`;
+
+    try {
+      const res = await axios.post(endpoint, { test: "#韓国語" });
+      setMessage(res.data.data.body);
+    } catch (e) {
+      setMessage("ライクに失敗しました");
+    }
+  };
+
   return (
     <div className="App">
       <Header />
       <TweetForm defaultValues={defaultValues} setMessage={setMessage} />
+      <button onClick={onLikeTweet}>Like</button>
       {message === "ツイートされました！！" && (
         <div className="status_bar">
           <Alert severity="success">{message}</Alert>
         </div>
       )}
+      {message === "ライクしました！！" && (
+        <div className="status_bar">
+          <Alert severity="success">{message}</Alert>
+        </div>
+      )}
       {message === "ツイートに失敗しました" && (
+        <div className="status_bar">
+          <Alert severity="error">{message}</Alert>
+        </div>
+      )}
+      {message === "ライクに失敗しました" && (
         <div className="status_bar">
           <Alert severity="error">{message}</Alert>
         </div>
